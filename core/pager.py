da8d0df5de1b00392054282f312583cd29d0778d
SouvickSarkar20 <souvicksarkarcbsa@gmail.com>
2026-02-24 17:20:33 +0530

    Working on the Pager Functionality

--- core/pager.py
+++ core/pager.py
@@ -2,3 +2,69 @@
 Pager (Your Disk Manager):
 Reads and writes 4KB pages to/from the .db file.
 """
+
+import os
+
+PAGE_SIZE = 4096
+
+class Pager:
+    def __init__(self, filename):
+        """
+        Open the database file. If it doesn't exist, it will be created.
+        We keep a dictionary `pages` as our memory cache.
+        """
+        self.filename = filename
+        
+        # open the file in binary read/write mode ("r+b"). 
+        # If it doesn't exist, we create it and then open it.
+        if not os.path.exists(filename):
+            open(filename, "w").close()
+            
+        self.file = open(filename, "r+b")
+        self.pages = {} # the cache: page_num -> bytes
+
+        # calculate how many pages currently exist in the file
+        self.file.seek(0, os.SEEK_END)
+        self.num_pages = self.file.tell() // PAGE_SIZE
+
+    def get_page(self, page_num):
+
+        if page_num in self.pages:
+            return self.pages[page_num]
+
+        # Otherwise, calculate where it sits on disk
+        offset = page_num * PAGE_SIZE
+        
+        # We might be asking for a brand new page at the very end of the file
+        if page_num >= self.num_pages:
+            # Create a brand new empty page filled with 0s
+            page = bytearray(PAGE_SIZE)
+            self.num_pages += 1
+        else:
+            # Seek to the correct offset and read 4KB
+            self.file.seek(offset)
+            page = bytearray(self.file.read(PAGE_SIZE))
+            
+        # Cache it for next time
+        self.pages[page_num] = page
+        return page
+
+    def flush_page(self, page_num):
+        """
+        Write a specific page from memory back to the disk.
+        """
+        if page_num in self.pages:
+            offset = page_num * PAGE_SIZE
+            self.file.seek(offset)
+            self.file.write(self.pages[page_num])
+            # Ask the OS to actually write to disk immediately (optional but good)
+            self.file.flush()
+
+    def close(self):
+        """
+        Close the pager, making sure to flush all cached pages back to disk first.
+        """
+        for page_num in self.pages.keys():
+            self.flush_page(page_num)
+        self.file.close()
+
